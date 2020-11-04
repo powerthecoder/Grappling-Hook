0418f26adc506849440aae3a483fd34d5c24bac4
Leo Power <68101457+powerthecoder@users.noreply.github.com>
2020-11-03 23:57:59 -0700

    Update Main.java

--- src/xyz/powerthecoder/GrapplingHook/Main.java
+++ src/xyz/powerthecoder/GrapplingHook/Main.java
@@ -1,7 +1,12 @@
 package xyz.powerthecoder.GrapplingHook;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
 import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -15,6 +20,7 @@ import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
@@ -23,8 +29,12 @@ import net.md_5.bungee.api.ChatColor;
 
 public class Main extends JavaPlugin implements Listener {
 	
+	// CoolDown HashMap
+	Map<String, Long> cooldowns = new HashMap<String, Long>();
+	
 	@Override
 	public void onEnable() {
+		Bukkit.addRecipe(getRecipe());
 		this.getServer().getPluginManager().registerEvents(this, this);
 	}
 	
@@ -55,7 +65,30 @@ public class Main extends JavaPlugin implements Listener {
 		return false;
 	}
 	
+	public ShapedRecipe getRecipe() {
+		// Custom Crafting Table
+		ItemStack hook = new ItemStack(Material.IRON_PICKAXE);
+		ItemMeta meta = hook.getItemMeta();
+		
+		meta.setDisplayName(ChatColor.GOLD + "Grappling Hook");
+		meta.addEnchant(Enchantment.DURABILITY, 1, true);
+		meta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES);
+		meta.addItemFlags(ItemFlag.HIDE_ENCHANTS);
+		
+		hook.setItemMeta(meta);
+		
+		NamespacedKey key = new NamespacedKey(this, "grappling_hook");
+		ShapedRecipe recipe = new ShapedRecipe(key, hook);
+		recipe.shape("SSS", " I ", " W ");
+		recipe.setIngredient('S', Material.STRING);
+		recipe.setIngredient('I', Material.IRON_INGOT);
+		recipe.setIngredient('W', Material.STICK);
+		return recipe;
+	}
+	
+	
 	public ItemStack getItem() {
+		// Create Grappling Hook
 		ItemStack hook = new ItemStack(Material.IRON_PICKAXE);
 		ItemMeta meta = hook.getItemMeta();
 		
@@ -72,7 +105,20 @@ public class Main extends JavaPlugin implements Listener {
 	@EventHandler()
 	public void onPunch(PlayerInteractEvent event) {
 		Player player = event.getPlayer();
+		// On Click and check if item is hook
 		if (player.getEquipment().getItemInMainHand().hasItemMeta() && player.getEquipment().getItemInMainHand().getItemMeta().getDisplayName().equals(ChatColor.GOLD + "Grappling Hook") && player.getEquipment().getItemInMainHand().getType().equals(Material.IRON_PICKAXE)){
+			// Cooldown
+			if (cooldowns.containsKey(player.getName())) {
+				// Player is inside HashMap
+				if (cooldowns.get(player.getName()) > System.currentTimeMillis()) {
+					// still on cooldown
+					long timeleft = (cooldowns.get(player.getName()) - System.currentTimeMillis() / 1000);
+					player.sendMessage(ChatColor.RED + "Hook ready in " + timeleft + " second(s)");
+				}
+			}
+			cooldowns.put(player.getName(), System.currentTimeMillis() + (5 * 1000));
+			
+			// Teleport
 			Block block = player.getTargetBlock(null, 10);
 			Location loc = block.getLocation();
 			player.teleport(loc);
